60ec15b9dfb77881ae9715717f4090fd8a5a1c33
Pascal <pascal.reinhardt@gmx.net>
2023-09-03 14:48:26 +0200

    Rechenoperationen hinzugefügt.

--- src/Rechenoperationen.java
+++ src/Rechenoperationen.java
@@ -1,4 +1,28 @@
 public class Rechenoperationen {
 
+    private static double ergebnis = 0;
+
+
+
+
+    private static double add(double x, double y){
+        ergebnis = x + y;
+        return ergebnis;
+    }
+
+    private static double sub(double x, double y){
+        ergebnis = x - y;
+        return ergebnis;
+    }
+
+    private static double multiply(double x, double y){
+        ergebnis = x * y;
+        return ergebnis;
+    }
+
+    private static double divide(double x, double y){
+        ergebnis = x / y;
+        return ergebnis;
+    }
 
 }
